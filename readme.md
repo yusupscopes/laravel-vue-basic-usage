ad63b48893eb14dcb18ce1da3ca78966410d2774
Yusup Maulana <yusupmaulana950@gmail.com>
2018-10-28 09:44:44 +0700

    Passing through props

---
+++ readme.md
@@ -0,0 +1,6 @@
+# How To Use Vue.js in Laravel Application
+
+### There is 3 way:
+1. Passing through props (parameter in vue component)
+2. Through API endpoint
+3. Directly in blade (presentation file) view.
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">User Component</div>
+
+                    <div class="card-body">
+                        <ul>
+                            <li v-for="user in users" :key="user.id">{{user.email}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                users: [],
+            }
+        },
+        props: [
+            'params'
+        ],
+        mounted() {
+            this.users = this.params
+            console.log(this.users)
+        }
+    }
+</script>
